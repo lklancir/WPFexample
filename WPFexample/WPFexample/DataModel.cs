52e77e1a1a0a9ec59d24dacc71dfbe177366ba44
lklancir <klancir.luka92@gmail.com>
2015-02-07 16:31:10 +0100

    Simple Example

---
+++ WPFexample/WPFexample/DataModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFexample
+{
+    public class DataModel
+    {
+        public string apiResult = "n";
+
+        private void GetDataFromApi()
+        {
+            // Some web service
+            apiResult = "SOME RESULT FROM WEB API";
+        }
+    }
+}
---
+++ WPFexample/WPFexample/ViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFexample
+{
+    public class ViewModel
+    {
+        private DataModel dm = new DataModel();
+
+        public string ApiResult
+        {
+            get { return dm.apiResult; }
+            set { dm.apiResult = value; }
+        }
+    }
+}
